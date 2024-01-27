78f37f349965c11a6bc4b71bc6df2103e5912ead
Chebanu <0alexchebanu@gmail.com>
2024-01-27 17:03:09 +0200

    upd program and appsettings.json

--- UrlShortener.Api/Program.cs
+++ UrlShortener.Api/Program.cs
@@ -1,31 +1,69 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using PassGuardia.Domain.Commands;
-using UrlShortener.Domain.Algorithm;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using UrlShortener.Api.Constants;
+using UrlShortener.Api.Filters;
+using UrlShortener.Api.StartupExtensions;
+using UrlShortener.Domain.Constants;
 using UrlShortener.Domain.DbContexts;
+using UrlShortener.Domain;
+using System.IdentityModel.Tokens.Jwt;
+using UrlShortener.Domain.Algorithm;
 using UrlShortener.Domain.Repositories;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerConfiguration();
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<UrlShortenerDbContext>(options =>
+builder.Services.AddDbContext<UrlShortenerDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDomainServices(
+    builder.Configuration.GetConnectionString("AppDbConnection"),
+    builder.Configuration.GetSection("Jwt")
+);
+
+builder.Services
+    .AddAuthorizationBuilder()
+    .AddPolicy(AuthorizePolicies.Admin, policy => policy.RequireClaim(ClaimTypes.Role, Roles.Admin))
+    .AddPolicy(AuthorizePolicies.User, policy => policy.RequireClaim(ClaimTypes.Role, Roles.User));
+
+
+builder.Services
+    .AddAuthentication(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-}, ServiceLifetime.Scoped);
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+{
+    var jwtConfig = builder.Configuration.GetSection("Jwt");
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtConfig["Issuer"],
+        ValidAudience = jwtConfig["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig["Secret"])),
+        RoleClaimType = ClaimNames.Role,
+        NameClaimType = JwtRegisteredClaimNames.Name
+    };
+});
+
+ServiceValidatorConfiguration.AddValidatorConfiguration(builder.Services);
 
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddScoped<IUrlShortenerAlgoritm, UrlShortenerAlgoritm>();
-
-builder.Services.AddMediatR(c => c.RegisterServicesFromAssembly(typeof(CreateShortUrlCommand).Assembly));
-
-builder.Services.AddMvc(options =>
-{
-
-});
 
 var app = builder.Build();
 
@@ -35,6 +73,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseMiddleware<RequestAuditMiddleware>();
+
 app.MapControllers();
 
+
 await app.RunAsync();
+
+public partial class Program { }
